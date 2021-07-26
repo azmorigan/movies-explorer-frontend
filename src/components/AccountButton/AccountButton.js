d664975bc2295ec8ccbccc7fcfa6b8909eee633f
Rinchin <rin4in.2014@yandex.ru>
2021-07-26 10:59:33 +0300

    feat: add hovers

--- src/components/AccountButton/AccountButton.js
+++ src/components/AccountButton/AccountButton.js
@@ -3,8 +3,10 @@ import './AccountButton.css';
 
 function AccountButton(props) {
   return (
-    <Link className={`AccountButton ${props.className}`} to="/profile">
-      <p className="AccountButton__title">Аккаунт</p>
+    <Link
+      className={`AccountButton ${props.mainPage && 'AccountButton_type_mainPage'}`}
+      to="/profile">
+      <p className={`AccountButton__title ${props.mainPage && 'AccountButton__title_type_mainPage'}`}>Аккаунт</p>
       <div className="AccountButton__icon" />
     </Link>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,7 @@
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
-import { Link, Switch, Route, NavLink } from 'react-router-dom';
+import { Link, NavLink, Route, Switch } from 'react-router-dom';
 import AccountButton from '../AccountButton/AccountButton';
-import CloseButton from '../CloseButton/CloseButton';
-
-// Текущий роут
-// let pagePath = window.location.pathname === ('/movies' || '/profile' || '/saved-movies') && window.location.pathname;
-
 
 function Header(props) {
   return (
@@ -15,50 +10,56 @@ function Header(props) {
       <div className="Header__container">
         <Link className="Header__logo" to="/" />
 
-        <Switch>
 
-          <Route exact path="/">
-            {!props.loggedIn ?
-              <Navigation>
-                <Link className="Header__link" to="/signup">Регистрация</Link>
-                <Link className="Header__link Header__link_type_signIn" to="/signin">Войти</Link>
-              </Navigation> : <Link className="Header__link" to="/movies">В приложение &rarr;</Link>}
-          </Route>
+        {!props.loggedIn
 
-          <Route path={"/movies"}>
-            <Navigation>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/movies">Фильмы</NavLink>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/saved-movies">Сохраненные фильмы</NavLink>
-              <AccountButton />
-              <button
-                className="Header__burger"
-                onClick={props.openSidebar}></button>
-            </Navigation>
-          </Route>
+          ? <Navigation>
+            <Link className="Header__link" to="/signup">Регистрация</Link>
+            <Link className="Header__link Header__link_type_signIn" to="/signin">Войти</Link>
+          </Navigation>
 
-          <Route path={"/saved-movies"}>
-            <Navigation>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/movies">Фильмы</NavLink>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/saved-movies">Сохраненные фильмы</NavLink>
-              <AccountButton />
-              <button
-                className="Header__burger"
-                onClick={props.openSidebar}></button>
-            </Navigation>
-          </Route>
+          : <Navigation>
+            <Switch>
 
-          <Route path={"/profile"}>
-            <Navigation>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/movies">Фильмы</NavLink>
-              <NavLink className="Header__film" activeClassName="Header__film_opened" to="/saved-movies">Сохраненные фильмы</NavLink>
-              <AccountButton />
-              <button
-                className="Header__burger"
-                onClick={props.openSidebar}></button>
-            </Navigation>
-          </Route>
+              <Route exact path="/">
+                <NavLink
+                  className="Header__film Header__film_type_main"
+                  activeClassName="Header__film_opened"
+                  to="/movies">
+                  Фильмы
+                </NavLink>
+                <NavLink
+                  className="Header__film Header__film_type_main"
+                  activeClassName="Header__film_opened"
+                  to="/saved-movies">
+                  Сохраненные фильмы</NavLink>
+                <AccountButton mainPage={true} />
+                <button
+                  className="Header__burger"
+                  onClick={props.openSidebar}></button>
+              </Route>
 
-        </Switch>
+              <Route>
+                <NavLink
+                  className="Header__film"
+                  activeClassName="Header__film_opened"
+                  to="/movies">
+                  Фильмы
+                </NavLink>
+                <NavLink
+                  className="Header__film"
+                  activeClassName="Header__film_opened"
+                  to="/saved-movies">
+                  Сохраненные фильмы</NavLink>
+                <AccountButton />
+                <button
+                  className="Header__burger"
+                  onClick={props.openSidebar}></button>
+              </Route>
+
+            </Switch>
+          </Navigation>
+        }
 
       </div>
     </header >
